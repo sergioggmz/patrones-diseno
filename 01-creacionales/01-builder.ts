b48d1cdc7162d3dab44269ed964f980515abf136
Sergio <sergiodejaen1234@gmail.com>
2026-04-18 19:45:00 +0200

    FEAT: 01-creacionales - 01-builder

--- 01-creacionales/01-builder.ts
+++ 01-creacionales/01-builder.ts
@@ -12,3 +12,80 @@
  *
  * https://refactoring.guru/es/design-patterns/builder
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+class Computer {
+    public cpu: string = 'cpu - not defined';
+    public ram: string = 'ram - not defined';
+    public storage: string = 'storage - not defined';
+    public gpu?: string;
+
+    displayConfiguration() {
+        console.log(`
+        CPU: ${this.cpu}   
+        RAM: ${this.ram}   
+        Almacenamiento: ${this.storage}   
+        GPU: ${this.gpu ?? 'No tiene GPU'}   
+        `)
+    }
+}
+
+class ComputerBuilder {
+    private computer: Computer;
+
+    constructor() {
+        this.computer = new Computer();
+    }
+
+    setCPU(cpu: string):ComputerBuilder {
+        this.computer.cpu = cpu;
+        
+        return this;
+    }
+
+    setRAM(ram: string):ComputerBuilder {
+        this.computer.ram = ram;
+        
+        return this;
+    }
+
+    setStorage(storage: string):ComputerBuilder {
+        this.computer.storage = storage;
+        
+        return this;
+    }
+
+    setGPU(gpu: string):ComputerBuilder {
+        this.computer.gpu = gpu;
+        
+        return this;
+    }
+
+    build() {
+        return this.computer;
+    }
+
+}
+
+function main() {
+    const basicComputer = new ComputerBuilder()
+        .setCPU('Intel Core 2 Duo')
+        .setRAM('4GB')
+        .setStorage('256GB')
+        .build()
+        console.log('%cComputadora básica:',COLORS.blue);
+        basicComputer.displayConfiguration();
+        
+    const gamingComputer = new ComputerBuilder()
+        .setCPU('AMD Ryzen 7 5800X')
+        .setRAM('16GB DDR4')
+        .setStorage('1TB')
+        .setGPU('NVIDIA RTX 3060')
+        .build()
+        console.log('%c\nComputadora gamer\n',COLORS.cyan);
+        gamingComputer.displayConfiguration()
+        
+}
+
+main();
--- 01-creacionales/01.2-builder.ts
+++ 01-creacionales/01.2-builder.ts
@@ -11,7 +11,7 @@
  * * que lo componen.
  */
 
-import { COLORS } from '../helpers/colors.ts';
+import { COLORS } from "../helpers/colors.ts";
 
 //! Tarea: crear un QueryBuilder para construir consultas SQL
 /**
@@ -50,37 +50,49 @@ class QueryBuilder {
   }
 
   select(...fields: string[]): QueryBuilder {
-    throw new Error('Method not implemented.');
+    this.fields = fields;
+    return this;
   }
 
   where(condition: string): QueryBuilder {
-    throw new Error('Method not implemented.');
+    this.conditions.push(condition);
+    return this;
   }
 
-  orderBy(field: string, direction: 'ASC' | 'DESC' = 'ASC'): QueryBuilder {
-    throw new Error('Method not implemented.');
+  orderBy(field: string, direction: "ASC" | "DESC" = "ASC"): QueryBuilder {
+    this.orderFields.push(`${field} ${direction}`);
+
+    return this;
   }
 
   limit(count: number): QueryBuilder {
-    throw new Error('Method not implemented.');
+    this.limitCount = count;
+    return this;
   }
 
   execute(): string {
-    // Select id, name, email from users where age > 18 and country = 'Cri' order by name ASC limit 10;
-    throw new Error('Method not implemented.');
+    const fields = this.fields.length > 0 ? this.fields.join(', '): '*';
+    const whereClause = this.conditions.length > 0
+      ? `WHERE ${this.conditions.join(' AND ')}`
+      : '';
+    const orderByClause = this.orderFields.length > 0
+    ? `ORDER BY ${this.orderFields.join(', ')}`
+    : '';
+    const limitClause = this.limitCount ? `LIMIT ${this.limitCount}` : '';
+    return `Select ${fields} from ${this.table} ${whereClause} ${orderByClause} ${limitClause}`
   }
 }
 
 function main() {
-  const usersQuery = new QueryBuilder('users')
-    .select('id', 'name', 'email')
-    .where('age > 18')
-    .where("country = 'Cri'") // Esto debe de hacer una condición AND
-    .orderBy('name', 'ASC')
-    .limit(10)
+  const usersQuery = new QueryBuilder("users")
+    .select("id", "name", "email")
+    .where("age > 20")
+    .where("country = 'ESP'") // Esto debe de hacer una condición AND
+    .orderBy("age", "DESC")
+    .limit(100)
     .execute();
 
-  console.log('%cConsulta:\n', COLORS.red);
+  console.log("%cConsulta:\n", COLORS.red);
   console.log(usersQuery);
 }
 
