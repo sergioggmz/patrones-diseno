aa3ac957a87ea2775514786384ba3491adf902e6
Sergio <sergiodejaen1234@gmail.com>
2026-04-26 20:34:32 +0200

    FEAT: Implement Prototype pattern with Document and Pokemon classes

--- 01-creacionales/04-prototype.ts
+++ 01-creacionales/04-prototype.ts
@@ -9,3 +9,41 @@
  * 
  * https://refactoring.guru/es/design-patterns/prototype
  */
+
+class Document {
+  public title: string;
+  private content: string;
+  public author: string;
+
+  constructor(title: string, content: string, author: string) {
+    this.title = title;
+    this.content = content;
+    this.author = author;
+  }
+
+  clone(): Document {
+    return new Document(this.title,this.content,this.author);
+  }
+
+  displayInfo() {
+    console.log(
+      `
+                Title: ${this.title}    
+                Content: ${this.content}    
+                Author: ${this.author}    
+            `,
+    );
+  }
+}
+
+function main() {
+  const document1 = new Document("Cotización", "500 dolores", "Sergio");
+  console.log({ document1 });
+  document1.displayInfo();
+  const document2 = document1.clone();
+  document2.title = "Nueva cotización";
+  console.log({ document2 });
+  document2.displayInfo();
+}
+
+main();
--- 01-creacionales/04.2-prototype.ts
+++ 01-creacionales/04.2-prototype.ts
@@ -10,6 +10,8 @@
  * https://refactoring.guru/es/design-patterns/prototype
  */
 
+import { COLORS } from "../helpers/colors.ts";
+
 class Pokemon {
   name: string;
   type: string;
@@ -17,20 +19,23 @@ class Pokemon {
   attacks: string[];
 
   constructor(name: string, type: string, level: number, attacks: string[]) {
-    throw new Error('Method not implemented.');
+    this.name = name;
+    this.type = type;
+    this.level = level;
+    this.attacks = attacks;
   }
 
   // Método para clonar el Pokémon
   clone(): Pokemon {
     // Los ataques deben de evitar pasarse por referencia, es decir, no deben de ser el mismo arreglo.
-    // Completar: Debe devolver un nuevo Pokémon con los mismos atributos
+    return new Pokemon(this.name, this.type, this.level, [...this.attacks]);
   }
 
   displayInfo(): void {
     console.log(
       `Nombre: ${this.name}\nTipo: ${this.type}\nNivel: ${
         this.level
-      }\nAtaques: ${this.attacks.join(', ')}`
+      }\nAtaques: ${this.attacks.join(", ")} \n`,
     );
   }
 }
@@ -41,11 +46,24 @@ class Pokemon {
 // 3. Llamar a displayInfo en cada Pokémon para mostrar sus detalles.
 
 // Ejemplo:
-// const basePokemon = new Pokemon("Charmander", "Fuego", 1, ["Llamarada", "Arañazo"]);
-// const clone1 = basePokemon.clone();
-// clone1.name = "Charmeleon";
-// clone1.level = 16;
-// clone1.attacks.push("Lanzallamas");
 
-// basePokemon.displayInfo(); // Aquí no debe de aparecer "Lanzallamas"
-// clone1.displayInfo();
+function main() {
+  const basePokemon = new Pokemon("Charmander", "Fuego", 1, [
+    "Llamarada",
+    "Arañazo",
+  ]);
+  const clone1 = basePokemon.clone();
+  clone1.name = "Charmeleon";
+  clone1.level = 16;
+  clone1.attacks.push("Lanzallamas");
+
+  console.log('%cCharmander', COLORS.red);
+  
+  basePokemon.displayInfo(); // Aquí no debe de aparecer "Lanzallamas"
+
+  console.log('%cCharmeleon', COLORS.pink);
+  
+  clone1.displayInfo();
+}
+
+main();
