eed02fbf7d92d2c11962881aca394a5094db8850
Sergio <sergiodejaen1234@gmail.com>
2026-05-23 14:08:39 +0200

    feat: inmutabilidad

--- 01-creacionales/05.2-inmutabilidad.ts
+++ 01-creacionales/05.2-inmutabilidad.ts
@@ -17,19 +17,26 @@
 
 import { COLORS } from '../helpers/colors.ts';
 
+interface PlayerProps {
+  name: string;
+  score: number;
+  level: number;
+}
 // 1. Clase Player inmutable
 class Player {
   readonly name: string;
   readonly score: number;
   readonly level: number;
 
-  constructor(name: string, score: number, level: number) {
-    throw new Error('Method not implemented.');
+  constructor({level, name, score}: PlayerProps) {
+    this.name = name;
+    this.score = score;
+    this.level = level;
   }
 
   // Método copyWith para crear una copia modificada del jugador
-  copyWith({ name, score, level }: Partial<Player>): Player {
-    throw new Error('Method not implemented.');
+  copyWith({ name, score, level}: Partial<Player>): Player {
+    return new Player({level: level ?? this.level, name: name ?? this.name, score: score ?? this.score});
   }
 
   displayState(): void {
@@ -42,7 +49,7 @@ class Player {
 // 2. Código Cliente para probar
 function main() {
   // Crear jugador inicial
-  let player = new Player('Carlos', 0, 1);
+  let player = new Player({ name: 'Carlos', score: 0, level: 1 });
   console.log('Estado inicial:');
   player.displayState();
 
