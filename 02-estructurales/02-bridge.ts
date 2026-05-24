9b9982da43fde4559f9b5e242b5506a14c7a1718
Sergio <sergiodejaen1234@gmail.com>
2026-05-24 14:08:30 +0200

    feat: pattern bridge

--- 02-estructurales/02-bridge.ts
+++ 02-estructurales/02-bridge.ts
@@ -9,3 +9,78 @@
  *
  * https://refactoring.guru/es/design-patterns/bridge
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+interface Ability {
+    use(): void;
+}
+
+class SwordAttack implements Ability {
+    use(): void {
+        console.log('Ataca con una %cespada ferozmente', COLORS.blue);
+    }
+}
+class AxeAttack implements Ability {
+    use(): void {
+        console.log('Ataca con una %chacha brutalmente', COLORS.red);
+    }
+}
+
+class MagicSpell implements Ability {
+    use(): void {
+        console.log('Lanza un hechizo %cmágico poderoso', COLORS.green);
+    }
+}
+class FireballSpell implements Ability {
+    use(): void {
+        console.log('Lanza una bola de fuego %cpoderosa', COLORS.green);
+    }
+}
+
+abstract class Character {
+    protected ability: Ability;
+
+    constructor(ability: Ability) {
+        this.ability = ability;
+    }
+
+    setAbility(ability: Ability): void {
+        this.ability = ability;
+    }
+    abstract performAbility(): void;
+}
+
+class Warrior extends Character {
+
+
+    override performAbility(): void {
+        console.log('El guerrero está listo para luchar');
+        this.ability.use();
+    }
+
+}
+class Wizard extends Character {
+
+
+    override performAbility(): void {
+        console.log('\nEl mago prepara su magia');
+        this.ability.use();
+    }
+
+}
+
+function main () {
+    const warrior = new Warrior(new SwordAttack());
+
+    warrior.performAbility();
+    warrior.setAbility(new AxeAttack());
+    warrior.performAbility();
+
+    const wizard = new Wizard(new MagicSpell());
+    wizard.performAbility();
+    wizard.setAbility(new FireballSpell());
+    wizard.performAbility();
+}
+
+main();
--- 02-estructurales/02.2-bridge.ts
+++ 02-estructurales/02.2-bridge.ts
@@ -41,49 +41,48 @@ class PushNotificationChannel implements NotificationChannel {
 // Define la propiedad `channel` y el método `notify`
 
 abstract class Notification {
-  // TODO: Definir la propiedad `channel` de tipo NotificationChannel
-  // TODO: Definir el constructor de la clase
-  // TODO: Definir el método `notify` y `setChannel` (abstractos)
+  protected channel: NotificationChannel;
+
+  constructor(channel: NotificationChannel) {
+    this.channel = channel;
+  }
+
+  abstract notify(message: string): void;
+  abstract setChannel(channel: NotificationChannel): void;
 }
 
 // 4. Clases Concretas de Notificaciones
 
 class AlertNotification extends Notification {
-  notify(message: string): void {
+  override notify(message: string): void {
     console.log('\n%cNotificación de Alerta:', COLORS.red);
-    // TODO: Enviar el mensaje a través del canal
-    throw new Error('Method not implemented.');
+    this.channel.send(message);
   }
 
-  setChannel(channel: NotificationChannel): void {
-    // TODO: Asignar el canal a la propiedad `channel`
-    throw new Error('Method not implemented.');
+  override setChannel(channel: NotificationChannel): void {
+    this.channel = channel;
   }
 }
 
 class ReminderNotification extends Notification {
-  notify(message: string): void {
+  override notify(message: string): void {
     console.log('\n%cNotificación de Recordatorio:', COLORS.blue);
-    // TODO: Enviar el mensaje a través del canal
-    throw new Error('Method not implemented.');
+    this.channel.send(message);
   }
 
-  setChannel(channel: NotificationChannel): void {
-    // TODO: Asignar el canal a la propiedad `channel`
-    throw new Error('Method not implemented.');
+  override setChannel(channel: NotificationChannel): void {
+    this.channel = channel;
   }
 }
 
 class PushNotification extends Notification {
   override notify(message: string): void {
     console.log('\n%cNotificación de Push:', COLORS.green);
-    // TODO: Enviar el mensaje a través del canal
-    throw new Error('Method not implemented.');
+    this.channel.send(message);
   }
 
   override setChannel(channel: NotificationChannel): void {
-    // TODO: Asignar el canal a la propiedad `channel`
-    throw new Error('Method not implemented.');
+    this.channel = channel;
   }
 }
 
