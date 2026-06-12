8e5eb78efd35ee321b31463059ea61d1a4dfac58
Sergio <sergiodejaen1234@gmail.com>
2026-06-12 18:55:04 +0200

    feat: facade exercise implemented

--- 02-estructurales/05-facade.ts
+++ 02-estructurales/05-facade.ts
@@ -11,3 +11,117 @@
  *
  * https://refactoring.guru/es/design-patterns/facade
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+class Projector {
+  turnOn() {
+    console.log("Proyector encendido");
+  }
+
+  turnOff() {
+    console.log("Proyector apagado");
+  }
+}
+
+class SoundSystem {
+  on() {
+    console.log("Sistema de sonido encendido");
+  }
+
+  off() {
+    console.log("Sistema de sonido apagado");
+  }
+}
+
+class VideoPlayer {
+  on() {
+    console.log("Video player encendido");
+  }
+
+  play(movie: string) {
+    console.log(`Reproduciendo %c${movie}`, COLORS.blue);
+  }
+
+  stop() {
+    console.log("Película detenida");
+  }
+
+  off() {
+    console.log("Video player apagado");
+  }
+}
+
+class PopcornMaker {
+  poppingPopcorn() {
+    console.log("Haciendo palómitas");
+  }
+
+  turnOffPoppingPopcorn() {
+    console.log("Deteniendo las palomitas");
+  }
+}
+
+interface HomeTheaterFacadeOptions {
+  projector: Projector;
+  soundSystem: SoundSystem;
+  videoPlayer: VideoPlayer;
+  popcornMaker: PopcornMaker;
+}
+
+class HomeTheaterFacade {
+  private projector: Projector;
+  private soundSystem: SoundSystem;
+  private videoPlayer: VideoPlayer;
+  private popcornMaker: PopcornMaker;
+
+  constructor({
+    projector,
+    soundSystem,
+    videoPlayer,
+    popcornMaker,
+  }: HomeTheaterFacadeOptions) {
+    this.projector = projector;
+    this.soundSystem = soundSystem;
+    this.videoPlayer = videoPlayer;
+    this.popcornMaker = popcornMaker;
+  }
+
+  watchMovie(movie: string): void {
+    console.log("%cPreparando para ver la película", COLORS.blue);
+    this.projector.turnOn();
+    this.soundSystem.on();
+    this.popcornMaker.poppingPopcorn();
+    this.videoPlayer.on();
+    this.videoPlayer.play(movie);
+
+    console.log("%cDisfrute de la película", COLORS.blue);
+  }
+
+  endWatchingMovie(): void {
+    console.log("%c\n\nPreparando para detener la película", COLORS.blue);
+    this.projector.turnOff();
+    this.soundSystem.off();
+    this.popcornMaker.turnOffPoppingPopcorn();
+    this.videoPlayer.stop();
+    this.videoPlayer.off();
+
+    console.log("%cSistema apagado\n", COLORS.blue);
+  }
+}
+function main() {
+  const projector = new Projector();
+  const soundSystem = new SoundSystem();
+  const videoPlayer = new VideoPlayer();
+  const popcornMaker = new PopcornMaker();
+
+  const homeTheaterFacade = new HomeTheaterFacade({
+    projector,
+    soundSystem,
+    videoPlayer,
+    popcornMaker,
+  });
+  homeTheaterFacade.watchMovie("Avengers Endgame");
+  homeTheaterFacade.endWatchingMovie();
+}
+main();
