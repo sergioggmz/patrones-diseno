e8c1f3a9404b19fa85150ffc8c050441dbaef35e
Sergio Guil Gómez <sergiodejaen1234@gmail.com>
2026-06-14 22:15:27 +0200

    feat: comman pattern

--- 03-comportamiento/02-command.ts
+++ 03-comportamiento/02-command.ts
@@ -12,3 +12,121 @@
  *
  *
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+interface Command {
+  execute(): void;
+}
+
+class Light {
+  turnOn(): void {
+    console.log("%cLa luz está encendida", COLORS.yellow);
+  }
+
+  turnOff(): void {
+    console.log("%cLa luz está apagada", COLORS.yellow);
+  }
+}
+class Fan {
+  turnOn(): void {
+    console.log("%cEl ventilador está encendido", COLORS.green);
+  }
+
+  turnOff(): void {
+    console.log("%cEl ventilador está apagado", COLORS.green);
+  }
+}
+
+// Comandos
+
+class LightOnCommand implements Command {
+  constructor(private light: Light) {}
+
+  execute(): void {
+    this.light.turnOn();
+  }
+}
+class LightOffCommand implements Command {
+  constructor(private light: Light) {}
+
+  execute(): void {
+    this.light.turnOff();
+  }
+}
+class FanOnCommand implements Command {
+  constructor(private fan: Fan) {}
+
+  execute(): void {
+    this.fan.turnOn();
+  }
+}
+class FanOffCommand implements Command {
+  constructor(private fan: Fan) {}
+
+  execute(): void {
+    this.fan.turnOff();
+  }
+}
+
+class RemoteControl {
+  private commands: Record<string, Command> = {};
+
+  setCommand(button: string, command: Command) {
+    this.commands[button] = command;
+  }
+
+  pressButton(button: string): void {
+    if (this.commands[button]) {
+      this.commands[button].execute();
+      return;
+    }
+
+    console.log("%cNo se ha asignado un comando a ese botón", COLORS.red);
+  }
+}
+
+function main() {
+  const remoteControl = new RemoteControl();
+  const light = new Light();
+  const fan = new Fan();
+
+  // Crear los comandos para los dispositivos
+  const lightOnCommand = new LightOnCommand(light);
+  const lightOffCommand = new LightOffCommand(light);
+
+  const fanOnCommand = new FanOnCommand(fan);
+  const fanOffCommand = new FanOffCommand(fan);
+
+  // Asignar las acciones al control remoto
+  remoteControl.setCommand("1", lightOnCommand);
+  remoteControl.setCommand("2", lightOffCommand);
+  remoteControl.setCommand("3", fanOnCommand);
+  remoteControl.setCommand("4", fanOffCommand);
+
+  let continueProgram = true;
+
+  do {
+    console.clear();
+    const pressedButton =
+      prompt(
+        `Presiona un botón del control: 
+      1. Encender la luz 
+      2. Apagar la luz 
+      3. Encender ventilador 
+      4. Apagar ventilador
+      
+      Botón: 
+      `,
+      ) ?? "";
+
+    remoteControl.pressButton(pressedButton);
+
+    const continueProgramResponse = prompt(
+      `\n ¿Deseas continuar? (y/n):`,
+    )?.toLowerCase();
+
+    continueProgram = continueProgramResponse === "n" ? false : true;
+  } while (continueProgram);
+}
+main();
--- 03-comportamiento/02.2-command.ts
+++ 03-comportamiento/02.2-command.ts
@@ -12,7 +12,7 @@
  *
  */
 
-import { COLORS } from '../helpers/colors.ts';
+import { COLORS } from "../helpers/colors.ts";
 
 // 1. Interfaz Command
 interface Command {
@@ -22,8 +22,8 @@ interface Command {
 // 2. Clase Receptor - TextEditor
 
 class TextEditor {
-  private text: string = '';
-  private clipboard: string = '';
+  private text: string = "";
+  private clipboard: string = "";
   private history: string[] = [];
 
   // Agregar texto al editor
@@ -37,7 +37,7 @@ class TextEditor {
     this.clipboard = this.text;
     console.log(
       `Texto copiado al portapapeles: \n%c"${this.clipboard}"`,
-      COLORS.blue
+      COLORS.blue,
     );
   }
 
@@ -56,7 +56,7 @@ class TextEditor {
       return;
     }
 
-    console.log('No hay nada para deshacer.');
+    console.log("No hay nada para deshacer.");
   }
 
   // Mostrar el texto actual
@@ -69,19 +69,34 @@ class TextEditor {
 class CopyCommand implements Command {
   private editor: TextEditor;
 
-  // TODO: Inyectar el editor en el constructor y el método execute con la acción respectiva
+  constructor(textEditor: TextEditor) {
+    this.editor = textEditor;
+  }
+  execute(): void {
+    this.editor.copy();
+  }
 }
 
 class PasteCommand implements Command {
   private editor: TextEditor;
 
-  // TODO: Inyectar el editor en el constructor y el método execute con la acción respectiva
+  constructor(textEditor: TextEditor) {
+    this.editor = textEditor;
+  }
+  execute(): void {
+    this.editor.paste();
+  }
 }
 
 class UndoCommand implements Command {
   private editor: TextEditor;
 
-  // TODO: Inyectar el editor en el constructor y el método execute con la acción respectiva
+  constructor(textEditor: TextEditor) {
+    this.editor = textEditor;
+  }
+  execute(): void {
+    this.editor.undo();
+  }
 }
 
 // 4. Clase Cliente - Toolbar
@@ -90,13 +105,14 @@ class Toolbar {
   private commands: Record<string, Command> = {};
 
   setCommand(button: string, command: Command): void {
-    // TODO: Asignar el comando al botón correspondiente
+    this.commands[button] = command;
   }
 
   clickButton(button: string): void {
-    //TODO: Ejecutar el comando correspondiente al botón
-
-    // TODO: Manejar el caso en que no haya un comando asignado al botón
+    if (this.commands[button]) {
+      this.commands[button].execute();
+      return;
+    }
     console.error(`No hay un comando asignado al botón "${button}"`);
   }
 }
@@ -113,36 +129,36 @@ function main() {
   const undoCommand = new UndoCommand(editor);
 
   // Asignar comandos a los botones de la barra de herramientas
-  toolbar.setCommand('copy', copyCommand);
-  toolbar.setCommand('paste', pasteCommand);
-  toolbar.setCommand('undo', undoCommand);
+  toolbar.setCommand("copy", copyCommand);
+  toolbar.setCommand("paste", pasteCommand);
+  toolbar.setCommand("undo", undoCommand);
 
   // Simulación de edición de texto
-  editor.type('H');
-  editor.type('o');
-  editor.type('l');
-  editor.type('a');
-  editor.type(' ');
-  editor.type('M');
-  editor.type('u');
-  editor.type('n');
-  editor.type('d');
-  editor.type('o');
-  editor.type('!');
+  editor.type("H");
+  editor.type("o");
+  editor.type("l");
+  editor.type("a");
+  editor.type(" ");
+  editor.type("M");
+  editor.type("u");
+  editor.type("n");
+  editor.type("d");
+  editor.type("o");
+  editor.type("!");
   console.log(`Texto actual: %c"${editor.getText()}"`, COLORS.green);
 
   // Usar la barra de herramientas
-  console.log('\nCopiando texto:');
-  toolbar.clickButton('copy');
+  console.log("\nCopiando texto:");
+  toolbar.clickButton("copy");
 
-  console.log('\nPegando texto:');
-  toolbar.clickButton('paste');
+  console.log("\nPegando texto:");
+  toolbar.clickButton("paste");
 
-  console.log('\nDeshaciendo la última acción:');
-  toolbar.clickButton('undo');
+  console.log("\nDeshaciendo la última acción:");
+  toolbar.clickButton("undo");
 
-  console.log('\nDeshaciendo de nuevo:');
-  toolbar.clickButton('undo');
+  console.log("\nDeshaciendo de nuevo:");
+  toolbar.clickButton("undo");
 
   console.log(`\nTexto final: "${editor.getText()}"`);
 }
