2fdd16d3f10f74c3715c7d56cca65a7cdb6d6e63
Sergio Guil Gómez <sergiodejaen1234@gmail.com>
2026-06-28 22:36:09 +0200

    feat: implement mediator pattern with ChatRoom and User classes

--- 03-comportamiento/04-mediator.ts
+++ 03-comportamiento/04-mediator.ts
@@ -9,3 +9,67 @@
  *
  * https://refactoring.guru/es/design-patterns/mediator
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+// Chatroom
+class ChatRoom {
+  private users: User[] = [];
+  public title: string;
+
+  constructor(title: string) {
+    this.title = title;
+  }
+
+  addUser(user: User): void {
+    this.users.push(user);
+  }
+
+  sendMessage(sender: User, message: string): void {
+    const usersToSend = this.users.filter((user) => user !== sender);
+    for (const user of usersToSend) {
+      user.receiveMessage(sender, message);
+    }
+  }
+}
+
+class User {
+  private username: string;
+  private chatRoom: ChatRoom;
+
+  constructor(username: string, chatRoom: ChatRoom) {
+    this.username = username;
+    this.chatRoom = chatRoom;
+    chatRoom.addUser(this);
+  }
+
+  sendMessage(message: string): void {
+    console.log(
+      `\n\n%c${this.username} envía: %c${message}`,
+      COLORS.blue,
+      COLORS.white,
+    );
+    this.chatRoom.sendMessage(this, message);
+  }
+
+  receiveMessage(sender: User, message: string): void {
+    console.log(
+      `\n\n%c${this.username} recibe de ${sender.username}: %c${message}`,
+      COLORS.blue,
+      COLORS.white,
+    );
+  }
+}
+
+function main() {
+  const chatRoom = new ChatRoom("Grupo de trabajo");
+  const user1 = new User("Sergio", chatRoom);
+  const user2 = new User("Juan", chatRoom);
+  const user3 = new User("Pedro", chatRoom);
+
+  user1.sendMessage("Hola a todos!");
+  user2.sendMessage("Hola Sergio, ¿cómo estás?");
+  user3.sendMessage("Hola Juan, Pedro, ¿cómo estáis?");
+  console.log("\n\n");
+}
+main();
--- 03-comportamiento/04.2-mediator.ts
+++ 03-comportamiento/04.2-mediator.ts
@@ -29,26 +29,34 @@
     notificándoles de la actividad de cada avión.
  */
 
-import { COLORS } from '../helpers/colors.ts';
+import { COLORS } from "../helpers/colors.ts";
 
 // Clase Mediador - ControlTower
 class ControlTower {
   private airplanes: Airplane[] = [];
 
   // Registrar un avión en la torre de control
-  // TODO: Implementar el método registerAirplane
-  // registerAirplane(airplane: Airplane)
+  registerAirplane(airplane: Airplane): void {
+    if (!this.airplanes.includes(airplane)) {
+      this.airplanes.push(airplane);
+    }
+  }
 
   // Enviar un mensaje de un avión a todos los demás
-  //TODO: Implementar el método sendMessage
-  // sendMessage(sender: Airplane, message: string): void
+  sendMessage(sender: Airplane, message: string): void {
+    this.airplanes
+      .filter((airplane) => airplane !== sender)
+      .forEach((airplane) => {
+        airplane.receiveMessage(sender, message);
+      });
+  }
 
   // Coordinación de aterrizaje
   requestLanding(sender: Airplane): void {
     console.log(
       `\n%cTorre de Control: %cPermiso de aterrizaje concedido a ${sender.getId()}`,
       COLORS.green,
-      COLORS.white
+      COLORS.white,
     );
 
     this.sendMessage(sender, `${sender.getId()} está aterrizando.`);
@@ -59,7 +67,7 @@ class ControlTower {
     console.log(
       `\n%cTorre de Control: %cPermiso de despegue concedido a ${sender.getId()}`,
       COLORS.green,
-      COLORS.white
+      COLORS.white,
     );
 
     this.sendMessage(sender, `${sender.getId()} está despegando.`);
@@ -75,7 +83,7 @@ class Airplane {
     this.id = id;
     this.controlTower = controlTower;
 
-    // TODO: Registrar el avión en la torre de control
+    this.controlTower.registerAirplane(this);
   }
 
   getId(): string {
@@ -86,21 +94,22 @@ class Airplane {
   requestLanding(): void {
     console.log(`${this.id} solicita permiso para aterrizar.`);
 
-    // TODO: Solicitar aterrizaje a la torre de control
+    this.controlTower.requestLanding(this);
   }
 
   // Solicitar despegue a la torre de control
   requestTakeoff(): void {
     console.log(`${this.id} solicita permiso para despegar.`);
 
-    // TODO: Solicitar despegue a la torre de control
+    this.controlTower.requestTakeoff(this);
   }
 
   // Recibir mensaje de otros aviones
   receiveMessage(sender: Airplane, message: string): void {
     console.log(
-      `${this.id} recibe mensaje de %c${sender.getId()}: "${message}"`,
-      COLORS.blue
+      `\n%c${this.id} recibe mensaje de ${sender.getId()}: %c"${message}"`,
+      COLORS.blue,
+      COLORS.white,
     );
   }
 }
@@ -110,9 +119,9 @@ class Airplane {
 function main(): void {
   const controlTower = new ControlTower();
 
-  const airplane1 = new Airplane('Vuelo 101', controlTower);
-  const airplane2 = new Airplane('Vuelo 202', controlTower);
-  const airplane3 = new Airplane('Vuelo 303', controlTower);
+  const airplane1 = new Airplane("Vuelo 101", controlTower);
+  const airplane2 = new Airplane("Vuelo 202", controlTower);
+  const airplane3 = new Airplane("Vuelo 303", controlTower);
 
   // Ejemplo de interacciones
   airplane1.requestLanding();
