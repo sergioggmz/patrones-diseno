800095a041ab937219d708cd5aef828fc41b7135
Sergio Guil Gómez <sergiodejaen1234@gmail.com>
2026-06-29 22:19:22 +0200

    feat: implement memento pattern for game state management

--- 03-comportamiento/05-memento.ts
+++ 03-comportamiento/05-memento.ts
@@ -8,3 +8,97 @@
  *
  * https://refactoring.guru/es/design-patterns/memento
  */
+import {COLORS} from "../helpers/colors.ts";
+
+class GameMemento {
+  private level: number;
+  private health: number;
+  private position: string;
+
+  constructor(level: number, health: number, position: string) {
+    this.level = level;
+    this.health = health;
+    this.position = position;
+  }
+
+  getLevel() {
+    return this.level;
+  }
+
+  getHealth() {
+    return this.health;
+  }
+
+  getPosition() {
+    return this.position;
+  }
+}
+
+class Game {
+  private level: number = 1;
+  private health: number = 100;
+  private position: string = 'inicio';
+
+  constructor() {
+    console.log(
+      `Jugando en el nivel ${this.level} salud: ${this.health} posición: ${this.position} `,
+    );
+  }
+
+  save(): GameMemento {
+    return new GameMemento(this.level, this.health, this.position);
+  }
+
+  play(level: number, health: number, position: string): void {
+    this.level = level;
+    this.health = health;
+    this.position = position;
+    console.log(`Jugando en el nivel ${this.level} salud: ${this.health} posición: ${this.position} `);
+  }
+  restore(memento: GameMemento): void {
+    this.level = memento.getLevel();
+    this.health = memento.getHealth();
+    this.position = memento.getPosition();
+
+    console.log(`\nProgreso restaurado
+      %cRestauración en el nivel ${this.level}
+      %csalud: ${this.health}
+      %cposición: ${this.position}
+    `, COLORS.yellow,COLORS.blue, COLORS.green)
+  }
+}
+
+class GameHistory {
+  private mementos: GameMemento[] = [];
+
+  push(memento: GameMemento) {
+    this.mementos.push(memento);
+  }
+
+  pop(): GameMemento | null {
+    return this.mementos.pop() ?? null;
+  }
+}
+
+function main() {
+  const game = new Game();
+  const history = new GameHistory();
+  history.push(game.save());
+  //Jugador avanza
+  game.play(2,90, 'Bosque Encantado');
+  history.push(game.save());
+
+  game.play(3,70,'Cueva Oscura');
+  history.push(game.save());
+
+  game.play(4,50, 'Castillo del Dragón');
+  console.log('%c\nEstado actual', COLORS.green)
+
+  game.restore(history.pop())
+  console.log('%c\nDespués de restaurar el último estado guardado ', COLORS.green)
+
+  game.restore(history.pop())
+  console.log('%c\nDespués de restaurar el último estado guardado ', COLORS.green)
+  console.log('\n\n')
+}
+main();
