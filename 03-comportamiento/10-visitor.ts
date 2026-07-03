cfa18ffe431d3be9813989e236f3c68098de36a5
Sergio Guil Gómez <sergiodejaen1234@gmail.com>
2026-07-03 22:59:32 +0200

    feat: implement Visitor pattern for theme park attractions with pricing and visitor types

--- 03-comportamiento/10-visitor.ts
+++ 03-comportamiento/10-visitor.ts
@@ -11,6 +11,8 @@
  * https://refactoring.guru/es/design-patterns/visitor
  */
 
+import { COLORS } from "../helpers/colors.ts";
+
 /**
  * Contexto: Imagina que estás diseñando un sistema para un parque
  * temático con diferentes tipos de atracciones:
@@ -24,3 +26,152 @@
  * de la atracción como del tipo de visitante,
  * sin modificar las clases originales.
  */
+
+interface Visitor {
+  visitRollerCoaster(rollerCoaster: RollerCoaster): void;
+  visitHauntedHouse(hauntedHouse: HauntedHouse): void;
+  visitFerrisWheel(ferrisWheel: FerrisWheel): void;
+}
+
+interface Attraction {
+  accept(visitor: Visitor): void;
+  getPrice(): number;
+}
+
+class RollerCoaster implements Attraction {
+  private price: number = 50;
+
+  getPrice(): number {
+    return this.price;
+  }
+
+  accept(visitor: Visitor): void {
+    visitor.visitRollerCoaster(this);
+  }
+}
+
+class HauntedHouse implements Attraction {
+  private price: number = 40;
+
+  getPrice(): number {
+    return this.price;
+  }
+
+  accept(visitor: Visitor): void {
+    visitor.visitHauntedHouse(this);
+  }
+}
+
+class FerrisWheel implements Attraction {
+  private price: number = 30;
+
+  getPrice(): number {
+    return this.price;
+  }
+
+  accept(visitor: Visitor): void {
+    visitor.visitFerrisWheel(this);
+  }
+}
+
+// Visitors
+class ChildVisitor implements Visitor {
+  visitRollerCoaster(rollerCoaster: RollerCoaster): void {
+    console.log(
+      `Niño en Montaña Rusa: Precio con descuento de $${
+        rollerCoaster.getPrice() * 0.5
+      }`,
+    );
+  }
+
+  visitHauntedHouse(hauntedHouse: HauntedHouse): void {
+    console.log(
+      `Niño en Casa del Terror: Precio con descuento de $${
+        hauntedHouse.getPrice() * 0.7
+      }`,
+    );
+  }
+
+  visitFerrisWheel(ferrisWheel: FerrisWheel): void {
+    console.log(
+      `Niño en la Rueda de la Fortuna: Precio con descuento de $${
+        ferrisWheel.getPrice() * 0.6
+      }`,
+    );
+  }
+}
+
+class AdultVisitor implements Visitor {
+  visitRollerCoaster(rollerCoaster: RollerCoaster): void {
+    console.log(
+      `Adulto en Montaña Rusa: Precio con descuento de $${rollerCoaster.getPrice()}`,
+    );
+  }
+
+  visitHauntedHouse(hauntedHouse: HauntedHouse): void {
+    console.log(
+      `Adulto en Casa del Terror: Precio con descuento de $${hauntedHouse.getPrice()}`,
+    );
+  }
+
+  visitFerrisWheel(ferrisWheel: FerrisWheel): void {
+    console.log(
+      `Adulto en la Rueda de la Fortuna: Precio con descuento de $${ferrisWheel.getPrice()}`,
+    );
+  }
+}
+
+class SeniorVisitor implements Visitor {
+  visitRollerCoaster(rollerCoaster: RollerCoaster): void {
+    console.log(
+      `Adulto en Montaña Rusa: Precio con descuento de $${
+        rollerCoaster.getPrice() * 0.85
+      }`,
+    );
+  }
+
+  visitHauntedHouse(hauntedHouse: HauntedHouse): void {
+    console.log(
+      `Adulto en Casa del Terror: Precio con descuento de $${
+        hauntedHouse.getPrice() * 0.85
+      }`,
+    );
+  }
+
+  visitFerrisWheel(ferrisWheel: FerrisWheel): void {
+    console.log(
+      `Adulto en la Rueda de la Fortuna: Precio con descuento de $${
+        ferrisWheel.getPrice() * 0.85
+      }`,
+    );
+  }
+}
+
+function main() {
+  const attractions: Attraction[] = [
+    new RollerCoaster(),
+    new HauntedHouse(),
+    new FerrisWheel(),
+  ];
+
+  console.log(`Montaña Rusa: ${new RollerCoaster().getPrice()}`);
+  console.log(`Casa del Terror: ${new HauntedHouse().getPrice()}`);
+  console.log(`La Rueda de la fortuna: ${new FerrisWheel().getPrice()}`);
+  console.log("\n\n");
+
+  console.log("\n%cVisitante Niño", COLORS.green);
+  const childVisitor = new ChildVisitor();
+  attractions.forEach((attraction) => attraction.accept(childVisitor));
+
+  console.log("\n%cVisitante Adulto", COLORS.purple);
+  const adultVisitor = new AdultVisitor();
+  attractions.forEach((attraction) => attraction.accept(adultVisitor));
+
+  console.log("\n%cVisitante Adulto Mayor", COLORS.purple);
+  const seniorVisitor = new SeniorVisitor();
+  attractions.forEach((attraction) => attraction.accept(seniorVisitor));
+
+  console.log("\n\n");
+}
+
+main();
--- 03-comportamiento/10.2-visitor.ts
+++ 03-comportamiento/10.2-visitor.ts
@@ -21,7 +21,7 @@
  * y verificar si los vehículos cumplen con las normas de emisión.
  */
 
-import { COLORS } from '../helpers/colors.ts';
+import { COLORS } from "../helpers/colors.ts";
 
 // Interfaz Visitor
 interface Visitor {
@@ -54,8 +54,7 @@ class Car implements Vehicle {
   }
 
   accept(visitor: Visitor): void {
-    // TODO: Implementar el método accept,
-    // que llama al método del visitor correspondiente
+    visitor.visitCar(this);
   }
 }
 
@@ -78,8 +77,7 @@ class Motorcycle implements Vehicle {
   }
 
   accept(visitor: Visitor): void {
-    // TODO: Implementar el método accept,
-    // que llama al método del visitor correspondiente
+    visitor.visitMotorcycle(this);
   }
 }
 
@@ -108,35 +106,32 @@ class Truck implements Vehicle {
   }
 
   accept(visitor: Visitor): void {
-    // TODO: Implementar el método accept,
-    // que llama al método del visitor correspondiente
+    visitor.visitTruck(this);
   }
 }
 
 // Clase visitante - MaintenanceCostVisitor
 class MaintenanceCostVisitor implements Visitor {
   visitCar(car: Car): void {
-    // TODO: Calcular el costo de mantenimiento para el automóvil
-    // cost = Kilómetros recorridos * 0.1 + (2024 - Año de fabricación) * 50
-
+    const cost = car.getKilometers() * 0.1 + (2024 - car.getYear()) * 50;
     console.log(
-      `Costo de mantenimiento para el automóvil: $${cost.toFixed(2)}`
+      `Costo de mantenimiento para el automóvil: $${cost.toFixed(2)}`,
     );
   }
 
   visitMotorcycle(motorcycle: Motorcycle): void {
-    // TODO: Calcular el costo de mantenimiento para la motocicleta
-    // cost = Kilómetros recorridos * 0.05 + (2024 - Año de fabricación) * 30
-
+    const cost =
+      motorcycle.getKilometers() * 0.05 + (2024 - motorcycle.getYear()) * 30;
     console.log(
-      `Costo de mantenimiento para la motocicleta: $${cost.toFixed(2)}`
+      `Costo de mantenimiento para la motocicleta: $${cost.toFixed(2)}`,
     );
   }
 
   visitTruck(truck: Truck): void {
-    //TODO: Calcular el costo de mantenimiento para el camión
-    // cost = Kilómetros recorridos * 0.15 + Capacidad de carga * 20 + (2024 - Año de fabricación) * 100
-
+    const cost =
+      truck.getKilometers() * 0.15 +
+      truck.getLoadCapacity() * 20 +
+      (2024 - truck.getYear()) * 100;
     console.log(`Costo de mantenimiento para el camión: $${cost.toFixed(2)}`);
   }
 }
@@ -144,21 +139,19 @@ class MaintenanceCostVisitor implements Visitor {
 // Clase visitante - EmissionCheckVisitor
 class EmissionCheckVisitor implements Visitor {
   visitCar(car: Car): void {
-    // TODO: Verificar si el automóvil cumple con las emisiones
-    // passes = Año de fabricación > 2000 && Kilómetros recorridos < 200_000
-    console.log(`Automóvil cumple con emisiones: ${passes ? 'Sí' : 'No'}`);
+    const passes = car.getYear() > 2000 && car.getKilometers() < 200_000;
+    console.log(`Automóvil cumple con emisiones: ${passes ? "Sí" : "No"}`);
   }
 
   visitMotorcycle(motorcycle: Motorcycle): void {
-    // TODO: Verificar si la motocicleta cumple con las emisiones
-    // passes = Año de fabricación > 2005 && Kilómetros recorridos < 100_000
-    console.log(`Motocicleta cumple con emisiones: ${passes ? 'Sí' : 'No'}`);
+    const passes =
+      motorcycle.getYear() > 2005 && motorcycle.getKilometers() < 100_000;
+    console.log(`Motocicleta cumple con emisiones: ${passes ? "Sí" : "No"}`);
   }
 
   visitTruck(truck: Truck): void {
-    // TODO: Verificar si el camión cumple con las emisiones
-    // passes = Año de fabricación > 2010 && Kilómetros recorridos < 300_000
-    console.log(`Camión cumple con emisiones: ${passes ? 'Sí' : 'No'}`);
+    const passes = truck.getYear() > 2010 && truck.getKilometers() < 300_000;
+    console.log(`Camión cumple con emisiones: ${passes ? "Sí" : "No"}`);
   }
 }
 
@@ -171,11 +164,11 @@ function main(): void {
     new Truck(2012, 250_000, 20),
   ];
 
-  console.log('%c\nCalculando costos de mantenimiento:', COLORS.green);
+  console.log("%c\nCalculando costos de mantenimiento:", COLORS.green);
   const maintenanceVisitor = new MaintenanceCostVisitor();
   vehicles.forEach((vehicle) => vehicle.accept(maintenanceVisitor));
 
-  console.log('%c\nVerificando emisiones:', COLORS.green);
+  console.log("%c\nVerificando emisiones:", COLORS.green);
   const emissionVisitor = new EmissionCheckVisitor();
   vehicles.forEach((vehicle) => vehicle.accept(emissionVisitor));
 }
